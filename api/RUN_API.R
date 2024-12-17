aaf6040f42e8c1574b36691ce11037768651ee6d
Gos2two <sergio@paigebraille.com>
2024-12-17 14:42:22 +0100

    feat: Docker image (1)

--- api/RUN_API.R
+++ api/RUN_API.R
@@ -1,11 +1,8 @@
-# Load the necessary library for creating REST APIs in R
+# Load the necessary libraries
 library(plumber)
 
-# Load the API script located at the specified path
-# The `plumb` function reads the R script and prepares it for use as an API.
+# Plumb the API file
 pr <- plumb("api/API.R")
 
 # Run the API on port 8000
-# The `run` method starts a local server on the specified port, allowing access to the API.
 pr$run(port = 8000)
-
